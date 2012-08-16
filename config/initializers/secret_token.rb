c04f2f700923ac76abcde1f2ba3eee6b042ced64
Triad Retail <cclayton@triadretail.com>
2012-08-16 11:51:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetheathApp::Application.config.secret_token = 'f5537eb33eea37c40f33822c2440c1ac22e29c853366be4f17025637c9c9c4f6fe1ffcb2d31cb4c72c112e157a48efbf628478712a43accf580393ec8fc4c205'
